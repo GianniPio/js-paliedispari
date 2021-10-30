4f1dd48f2853f84589ce1845e1eae71ae0d75592
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-30 16:05:33 +0200

    logical steps

--- palindrome/script.js
+++ palindrome/script.js
@@ -1,3 +1,18 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma.
 // Dare l’output relativo.
+
+// 1 Chidere all'utente un parola -> promt
+
+// 2 Controllare che la parola sia palindroma
+    // 2.1 Scomporre la parola in lettere -> split
+    // 2.2 Invertire le lettere
+    // 2.3 Ricomporre la parola
+    // 2.4 Controllare che le parole siano uguali -> if
+
+// 3 Dare l'output
+    // 3.1 Se le parole sono uguali sono palindrome
+    // 3.1 Altrimenti non sono palindrome
+
+
+
